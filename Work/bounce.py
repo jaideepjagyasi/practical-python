7f6d45db31e07fba19bbac4d260800cc273bce87
jaideep jagyasi <jaideep.jagyasi@gmail.com>
2020-06-01 15:30:18 +0530

    new changes

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+init_height=100 #meters
+
+for i in range(1,11):
+    init_height=init_height * (3/5)
+    # print(i,init_height)
+    # rounding upto4 digits
+    print(i,round(init_height,4))
